dbba34852ec854911eb3d4e1f748e81b63952843
alexhardo <alexhardo@msn.com>
2018-05-07 11:49:30 +0200

    finished second exercise

---
+++ manipulating_html_page_exercise_files/myscript.js
@@ -0,0 +1,18 @@
+$("form").submit(function(e) {
+
+    e.preventDefault();
+
+
+
+
+});
+
+// $("form").remove();
+
+$("article").prepend('<div>First Div</div>', '<div>Second Div</div>', '<div>Third Div</div>', '<div>Fourth Div</div>', '<div>Fifth Div</div>');
+
+$("#msg").val("search for.......");
+
+$("div").addClass("box");
+
+$("a").attr("href", "http://www.google.com/")
