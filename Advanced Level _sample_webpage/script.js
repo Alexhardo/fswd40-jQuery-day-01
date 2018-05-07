de2275f91ba7907455e422743053904cc7a5f9ec
alexhardo <alexhardo@msn.com>
2018-05-07 14:00:39 +0200

    finished third exercise

---
+++ Advanced Level _sample_webpage/script.js
@@ -0,0 +1,19 @@
+
+
+$("a").attr("href","http://www.codefactory.wien");
+
+$("p>code").css("color","red");
+$("ol>li").css("background-color","#2a7b90");
+$(".textbox").val("express you opinion").css("font-size","10px");
+
+$("img").attr("src", "https://upload.wikimedia.org/wikipedia/de/thumb/5/50/DFB-Logo_1945.svg/100px-DFB-Logo_1945.svg.png");
+$("Blockquote").css({"background-color":"orange","font-weight":"italic"});
+// $("Blockquote").css("font-weight","italic");
+
+$("#logo").css("color","black");
+$("span.gray").css("color","white");
+$("span.date").remove();
+$("ul.sidemenu").append( '<li>New Templates </li>','<li>Template </li>','<li> Contact Us </li>');
+$("div>p:first").text("When someone says: 'I want a programming language in which I n");
+
+// Be not afraid of life. Believe that life is worth living, and your belief will help create the fact." into "When someone says: 'I want a programming language in which I need only to say what I wish, and done', give him a lollipop.
