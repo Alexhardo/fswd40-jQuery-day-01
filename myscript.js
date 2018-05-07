baea9779da336c0c7c2dfffcf1ac6e61ea8cf461
alexhardo <alexhardo@msn.com>
2018-05-07 11:08:18 +0200

    finished fir exercise

--- myscript.js
+++ myscript.js
@@ -1,42 +1,43 @@
-/*
- * My jQuery selectors
- *
- * Apply a style such as a yellow background to the following elements.
- * Test your selectors one at a time.
- */
+// /*
+//  * My jQuery selectors
+//  *
+//  * Apply a style such as a yellow background to the following elements.
+//  * Test your selectors one at a time.
+//  */
 
-// all elements
- $("*").css("background-color", "yellow");
+// // all elements
+//  $("*").css("background-color", "yellow");
 
-// all p elements
-$("p").css("background-color","red");
+// // all p elements
+// $("p").css("background-color","red");
 
-// the p element with an ID of "intro"
-$("#intro").css("background-color","lightblue");
+// // the p element with an ID of "intro"
+// $("#intro").css("background-color","lightblue");
 
-// all elements with a class of "note"
-$(".note").css("color","white");
+// // all elements with a class of "note"
+// $(".note").css("color","white");
 
-// all DIV elements with a class of note
-$("div.note").css("color","blue");
+// // all DIV elements with a class of note
+// $("div.note").css("color","blue");
 
-// all p elements in the article element
-$("article>p").css("font-size","20px");
-$("article>p").css("font-weight","bold");
+// // all p elements in the article element
+// $("article>p").css("font-size","20px");
+// $("article>p").css("font-weight","bold");
 
-// the last element in any section which is a p tag
+// // the last element in any section which is a p tag
 
-$("p:last").css("background-color","green");
-// the first paragraph on the page
+// $("p:last").css("background-color","green");
+// // the first paragraph on the page
 
-$("p:first").css("background-color","green");
+// $("p:first").css("background-color","green");
 
-// all p elements which have a link
+// // all p elements which have a link
 
- $("p > [href]").css("font-weight","bold");
+//  $("p:has(a)").css("font-weight","bold");
 
-// the second li within nested ul element
+// // the second li within nested ul element
 
-
+// $("li:nth(2)").css("color","red");
 
 // every second and third p element
+$("p:nth-of-type(2), p:nth-of-type(3)").css("background-color","green");
